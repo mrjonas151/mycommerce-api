372c14b0a1d1a41e1c7f392df3d6e01ce8e04d9d
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-10-12 00:29:33 -0300

    feat: adding guia seller frontend route to cors

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,16 @@ import express from "express";
 import userRoutes from "./routes/userRoutes";
 import cors from "cors";
 import http from "http";
-import initWebSocketServer from "./websocket"; // Importar o arquivo websocket
+import initWebSocketServer from "./websocket"; 
 
 const app = express();
-const httpServer = http.createServer(app); // Criar servidor HTTP
+const httpServer = http.createServer(app); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: '*',
+    origin: 'https://guiaseller-frontend.dlmi5z.easypanel.host', 
+    credentials: true 
 }));
 
 app.use("/users", userRoutes);
