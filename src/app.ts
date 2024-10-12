ff2ec072fc7c86ac7251350d5ade262a01b2d0b2
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-10-12 02:26:04 -0300

    feat: update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,19 @@ import initWebSocketServer from "./websocket";
 const app = express();
 const httpServer = http.createServer(app); 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/users", userRoutes);
+app.options('*', cors()); 
+
 
 const io = initWebSocketServer(httpServer);
 
